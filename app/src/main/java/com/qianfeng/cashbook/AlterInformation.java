23ffd1329dccc0051baef838963d86cb01868feb
ccw0715 <939319200@qq.com>
2016-09-27 21:05:58 +0800

    最新

--- app/src/main/java/com/qianfeng/cashbook/AlterInformation.java
+++ app/src/main/java/com/qianfeng/cashbook/AlterInformation.java
@@ -1,12 +1,14 @@
 package com.qianfeng.cashbook;
 
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class AlterInformation extends AppCompatActivity {
 
@@ -15,6 +17,7 @@ public class AlterInformation extends AppCompatActivity {
     private int img[] = new int[]{R.drawable.a1, R.drawable.a2, R.drawable.a3, R.drawable.a4, R.drawable.a5,
                 R.drawable.a6, R.drawable.a7, R.drawable.a8, R.drawable.a9};
     private ImageView iv;
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +34,29 @@ public class AlterInformation extends AppCompatActivity {
 
     public void change(View view) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//        builder.setView()
+        View view1 = LayoutInflater.from(this).inflate(R.layout.mypiclayout, null);
+        RadioGroup rg = (RadioGroup) view1.findViewById(R.id.rg);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.a1:
+                        picId=R.drawable.a1;
+                        Toast.makeText(AlterInformation.this, "haha", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                        break;
+                }
+            }
+        });
+        builder.setView(view1);
+        dialog = builder.create();
+        dialog.show();
+        iv.setImageResource(picId);
 
+
+    }
+
+    public void save(View view) {
+        
     }
 }
